e5f53ac71f16bbad1e47b1d7254901f4e2387407
unknown <colleenbowes566@gmail.com>
2018-07-08 12:37:46 -0400

    first initialization for new portfolio

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" href="onepage-scroll.css">
+	<link rel="stylesheet" href="style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+
+	<script src="js/jquery.onepage-scroll.min.js" type="text/javascript"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+
+
+</head>
+<header>
+	<nav>
+	</nav>
+</header>
+
+<body>
+	<div class="main">
+		<section class="landing">
+			<h1>Colleen Bowes</h1>
+			<h2>Web Developer/Designer</h2>
+			<div class="socialMedia">
+				<div class="socialIcon">
+					<i class="fab fa-facebook-f" alt="Facebook"></i>
+				</div>
+				<div class="socialIcon">
+					<i class="fab fa-facebook-f" alt="Twitter"></i>
+				</div>
+				<div class="socialIcon">
+					<i class="fab fa-facebook-f" alt="Instagram"></i>
+				</div>
+				<div class="socialIcon">
+					<i class="fab fa-facebook-f" alt="Github"></i>
+				</div>
+				<div class="socialIcon">
+					<i class="fab fa-facebook-f" alt="LinkedIn"></i>
+				</div>
+			</div>
+		</section>
+
+		<section class="about">
+			<h1>Colleen Bowes</h1>
+			<h2>Web Developer/Designer</h2>
+		</section>
+		<section class="skills">
+			<h1>Colleen Bowes</h1>
+			<h2>Web Developer/Designer</h2>
+		</section>
+		<section class="work">
+			<h1>Colleen Bowes</h1>
+			<h2>Web Developer/Designer</h2>
+		</section>
+		<section class="contact">
+
+
+		</section>
+
+	</div>
+
+	<script src="js/scripts.js" type="text/javascript"></script>
+	<footer>
+	</footer>
+
+</body>
+
+</html>
